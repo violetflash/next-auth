9c76995e8aae60290e8551953358092cafb11306
Alex Grobovskiy <a.grobovsky@gmail.com>
2024-04-01 16:55:09 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+export default function Home() {
+  return (
+    <>
+      <p className="font-bold text-green-500">
+        Hello!
+      </p>
+      <Button size="lg" variant="outline">
+        Click Me!
+      </Button>
+    </>
+  );
+}
