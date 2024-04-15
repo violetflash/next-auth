33b40d2fb5cc43e416ebf29e2d02366ec95adf1c
Alex Grobovskiy <ktootaam@gmail.com>
2024-04-15 21:02:56 +0500

    add register form

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -1,7 +1,7 @@
 import { ExclamationTriangleIcon } from '@radix-ui/react-icons';
 
 interface FormErrorProps {
-  message: string;
+  message?: string;
 }
 
 export const FormError = ({ message }: FormErrorProps) => {
--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -86,17 +86,24 @@ FormItem.displayName = "FormItem"
 
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => {
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & { required?: boolean }
+>(({ className, required, ...props }, ref) => {
   const { error, formItemId } = useFormField()
 
   return (
-    <Label
-      ref={ref}
-      className={cn(error && "text-destructive", className)}
-      htmlFor={formItemId}
-      {...props}
-    />
+    <div className="flex gap-x-1 items-center">
+      <Label
+        ref={ref}
+        className={cn(
+          // error && "text-destructive",
+          className
+        )}
+        htmlFor={formItemId}
+        {...props}
+      />
+      { required && <span className="text-destructive">*</span>}
+    </div>
+
   )
 })
 FormLabel.displayName = "FormLabel"
